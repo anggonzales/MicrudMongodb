f8d1d3c5e6082fe92e9d7262e9fc1b1164702ac0
anggonzales <anggonzalesca@upt.pe>
2020-02-07 10:35:09 -0500

    Actualizar

---
+++ app/src/main/java/com/example/micrudmongodb/servicesRetrofit.java
@@ -0,0 +1,27 @@
+package com.example.micrudmongodb;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface servicesRetrofit {
+    @GET("products/test")
+    Call<String> getcheck();
+
+    @POST("products/create")
+    Call<String> newproducto(@Body Productos producto);
+
+    @GET("products/list")
+    Call<List<Productos>> getlist();
+
+    @GET("/products/buscar/{name}")
+    Call < Productos > getproducto(@Path("name") String name);
+
+    @PUT("/products/{id}/update")
+    Call<String> updateproducto(@Path("id") String id,@Body Productos producto);
+}
